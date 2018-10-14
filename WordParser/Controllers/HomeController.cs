9cd26964374f6978e3224b3c7dccb61d4697af09
murat <muratdemir.pau@gmail.com>
2018-10-14 13:16:44 +0300

    14.10.2018

---
+++ WordParser/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WordParser.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Parse(HttpPostedFileBase file)
+        {
+            string path = Path.Combine(Server.MapPath("~/files"),
+                                       Path.GetFileName(file.FileName));
+            file.SaveAs(path);
+            ViewBag.Message = "File uploaded successfully";
+
+            return View("Index");
+        }
+    }
+}
